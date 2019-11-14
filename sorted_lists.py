0a8bb4c99c8b1448ff4f23b8fbfed1575fd11c13
Vedant Paranjape <vedantparanjape160201@gmail.com>
2019-11-14 15:51:23 +0530

    changed sort_list.py to add own implementation of sort

--- sorted_lists.py
+++ sorted_lists.py
@@ -1,3 +1,16 @@
+def sort_list(list, reverse=False):
+    for i in range(0,len(list)):
+        for j in range(0, len(list)-i-1):
+            if list[j] > list[j+1] and not reverse:
+                a = list[j]
+                list[j] = list[j+1]
+                list[j+1] = a
+
+            elif list[j] < list[j+1] and reverse:
+                a = list[j]
+                list[j] = list[j+1]
+                list[j+1] = a
+
 option = int(input("1. enter a number \n2. enter a list \noption: "))
 
 if option == 1:
@@ -17,9 +30,9 @@ elif option == 2:
     number_list = [int(i) for i in input("enter a list of number: ").split(' ')]
 
     if len(number_list)%2 == 0:
-        number_list.sort()
+        sort_list(number_list)
         print(number_list)
 
     else:
-        number_list.sort(reverse=True)
+        sort_list(number_list, reverse=True)
         print(number_list)
